372e0a56be6f577e7b570b29f026541224d12e27
Olivier Giorgis <ogiorgis@logilab.fr>
2022-09-22 12:07:20 +0200

    fix: Don't stream python history at start

--- cmd.py
+++ cmd.py
@@ -6,18 +6,31 @@ APP_URL = "http://localhost:5000"
 SECRET_TOKEN = "azerty"
 
 
-def cmd_stream():
-    hl = readline.get_current_history_length()
-    last_cmd = readline.get_history_item(hl)
-    headers = {
-        "token": SECRET_TOKEN,
-        "Content-Type": "application/json",
-    }
-    data = {
-        "command": last_cmd,
-        "type": "python",
-    }
-    requests.post(f"{APP_URL}/command", headers=headers, data=json.dumps(data))
+class PythonStreamer:
+
+    def __init__(self):
+        self.start_streaming = False
+
+    def cmd_stream(self):
+        if not self.start_streaming:
+            self.start_streaming = True
+            return
+        hl = readline.get_current_history_length()
+        last_cmd = readline.get_history_item(hl)
+        headers = {
+            "token": SECRET_TOKEN,
+            "Content-Type": "application/json",
+        }
+        data = {
+            "command": last_cmd,
+            "type": "python",
+        }
+        requests.post(
+            f"{APP_URL}/command",
+            headers=headers,
+            data=json.dumps(data),
+        )
 
 
-readline.set_pre_input_hook(cmd_stream)
+streamer = PythonStreamer()
+readline.set_pre_input_hook(streamer.cmd_stream)
